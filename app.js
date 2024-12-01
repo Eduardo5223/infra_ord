f5984ebefcfcd88c86baccc8c7dcb245181e812c
Israel Eduardo Hernandez Valdez <97865469+Eduardo5223@users.noreply.github.com>
2024-12-01 17:43:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo desde azure infra');
+    res.send('Test de cambio');
 });
 
 app.listen(port, () => {
-    console.log('La aplicacione esta escuchando en http://localhost:${port}');
+    console.log('La aplicacione esta escuchando en ${port}');
 });
