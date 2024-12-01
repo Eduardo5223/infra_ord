89de170a073a85ee868daa4e68e20c1a6de2363e
Eduardo Hernandez <eduardo-h.v08@hotmail.com>
2024-12-01 17:26:15 -0600

    test

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo desde docker');
+    res.send('Hola Mundo desde azure');
 });
 
 app.listen(port, () => {
